122c49ab80f433ed72044232374df10fb1d8f946
Jan Heyd <jan.heyd@mtc.berlin>
2018-03-12 13:36:37 +0100

    improve failure messages for equals verification

--- src/main/java/de/janheyd/propertyverifier/feature/EqualsFeature.java
+++ src/main/java/de/janheyd/propertyverifier/feature/EqualsFeature.java
@@ -1,39 +1,76 @@
 package de.janheyd.propertyverifier.feature;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
 
 public class EqualsFeature<T, U> implements ObjectFeature<T, U> {
 
 	@Override
 	public void verify(Function<U, T> constructor, List<U> values) {
-		if (!objectsWithEqualPropertiesAreEqual(constructor, values))
-			throw new RuntimeException("Equals Verification failed");
-		if (!objectsWithDistinctPropertiesAreDistinct(constructor, values))
-			throw new RuntimeException("Equals Verification failed");
+		verifyObjectWithEqualPropertiesAreEqual(constructor, values);
+		verifyObjectsWithDistinctPropertiesAreDistinct(constructor, values);
 	}
 
-	private boolean objectsWithEqualPropertiesAreEqual(Function<U, T> constructor, List<U> values) {
-		return values.stream().allMatch(value -> constructor.apply(value).equals(constructor.apply(value)));
+	private void verifyObjectsWithDistinctPropertiesAreDistinct(Function<U, T> constructor, List<U> values) {
+		List<T> objects = values.stream().map(constructor).collect(toList());
+		List<T> distinct = distinct(objects);
+		if (distinct.size() != values.size()) {
+			String header = "Equals verification failed (distinct property values should not produce equal objects):\n";
+			String template = "object for property value %s is equal to object for property values %s";
+			String details = equalPairs(constructor, values).entrySet().stream()
+					.map(entry -> String.format(template, entry.getKey(), entry.getValue()))
+					.collect(joining("\n"));
+			throw new RuntimeException(header + details);
+		}
 	}
 
-	private boolean objectsWithDistinctPropertiesAreDistinct(Function<U, T> constructor, List<U> values) {
-		List<T> objects = values.stream().map(constructor).collect(Collectors.toList());
-		return countDistinct(objects) == values.size();
+	private Map<U, List<U>> equalPairs(Function<U, T> constructor, List<U> values) {
+		Map<U, List<U>> equalValues = new HashMap<>();
+		for (U value : values) {
+			List<U> collect = values.stream()
+					.filter(value2 -> !value.equals(value2))
+					.filter(value2 -> constructor.apply(value).equals(constructor.apply(value2)))
+					.collect(toList());
+			if (!collect.isEmpty()) {
+				equalValues.put(value, collect);
+			}
+		}
+		return equalValues;
+	}
+
+	private void verifyObjectWithEqualPropertiesAreEqual(Function<U, T> constructor, List<U> values) {
+		List<U> failedValues = values.stream()
+				.filter(value -> !constructor.apply(value).equals(constructor.apply(value)))
+				.collect(toList());
+		if (!failedValues.isEmpty()) {
+			String header = "Equals verification failed (equal property values should produce equal objects):\n";
+			String template = "object with property value %s is not equal to another object with same property value";
+			String details = failedValues.stream()
+					.map(value -> String.format(template, value))
+					.collect(joining("\n"));
+			throw new RuntimeException(header + details);
+		}
 	}
 
 	/*
 	 * Since we only verfiy equals and not hashCode we have to write our own method to count distinct elements
 	 * instead of relying on java built-ins like Stream::distinct (which uses hashCode)
 	 */
-	private int countDistinct(List<T> objects) {
+	private List<T> distinct(List<T> objects) {
 		List<T> distinct = new ArrayList<>();
-		for(T t: objects)
-			if(distinct.stream().noneMatch(t::equals))
+		for (T t : objects) {
+			if (distinct.stream().noneMatch(t::equals)) {
 				distinct.add(t);
-		return distinct.size();
+			}
+		}
+		return distinct;
 	}
 
 }
---
+++ src/test/java/de/janheyd/propertyverifier/feature/EqualsFeatureShould.java
@@ -0,0 +1,56 @@
+package de.janheyd.propertyverifier.feature;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+
+public class EqualsFeatureShould {
+
+	@Rule
+	public ExpectedException expected = ExpectedException.none();
+
+	@Test
+	public void produceErrorMessageForEqualPropertyProducesDistinctObjects() {
+		expected.expect(RuntimeException.class);
+		expected.expectMessage(equalTo(
+				"Equals verification failed (equal property values should produce equal objects):\n"
+						+ "object with property value 0 is not equal to another object with same property value\n"
+						+ "object with property value 1 is not equal to another object with same property value"
+		));
+
+		new EqualsFeature<HasDefaultEquals, Integer>().verify(HasDefaultEquals::new, asList(0, 1));
+	}
+
+	@Test
+	public void produceErrorMessageForDistinctPropertiesProduceEqualObjects() {
+		expected.expect(RuntimeException.class);
+		expected.expectMessage(equalTo(
+				"Equals verification failed (distinct property values should not produce equal objects):\n"
+						+ "object for property value 0 is equal to object for property values [1]\n"
+						+ "object for property value 1 is equal to object for property values [0]"
+		));
+
+		new EqualsFeature<HasConstantEquals, Integer>().verify(HasConstantEquals::new, asList(0, 1));
+	}
+
+	private class HasDefaultEquals {
+
+		public HasDefaultEquals(int x) {
+		}
+	}
+
+	private class HasConstantEquals {
+
+		public HasConstantEquals(int x) {
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			return true;
+		}
+	}
+}
