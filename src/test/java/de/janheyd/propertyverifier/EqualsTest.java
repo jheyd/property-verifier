0093b81ab8ea1241922afa075429bdca9e270e84
Jan Heyd <git@janheyd.de>
2018-03-12 16:01:17 +0100

    integrate ErrorFeature failure message verification in higher-level test

--- src/test/java/de/janheyd/propertyverifier/EqualsTest.java
+++ src/test/java/de/janheyd/propertyverifier/EqualsTest.java
@@ -1,8 +1,16 @@
 package de.janheyd.propertyverifier;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+
 
 public class EqualsTest {
+
+	@Rule
+	public ExpectedException expected = ExpectedException.none();
 
 	@Test
 	public void canVerifyGoodEquals() throws Exception {
@@ -12,16 +20,30 @@ public class EqualsTest {
 				.verify(0, 1);
 	}
 
-	@Test(expected = AssertionError.class)
+	@Test
 	public void canDetectConstantEquals() throws Exception {
+		expected.expect(AssertionError.class);
+		expected.expectMessage(equalTo(
+				"Equals verification failed (distinct property values should not produce equal objects):\n"
+						+ "object for property value 0 is equal to object for property values [1]\n"
+						+ "object for property value 1 is equal to object for property values [0]"
+		));
+
 		new PropertyVerifier()
 				.withConstructor(HasBadEquals::new)
 				.withEquals()
 				.verify(0, 1);
 	}
 
-	@Test(expected = AssertionError.class)
+	@Test
 	public void canDetectDefaultEquals() throws Exception {
+		expected.expect(AssertionError.class);
+		expected.expectMessage(equalTo(
+				"Equals verification failed (equal property values should produce equal objects):\n"
+						+ "object with property value 0 is not equal to another object with same property value\n"
+						+ "object with property value 1 is not equal to another object with same property value"
+		));
+
 		new PropertyVerifier()
 				.withConstructor(HasDefaultEquals::new)
 				.withEquals()
