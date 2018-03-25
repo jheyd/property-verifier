2866a2178420fbcecdcab6ce192ccf9203883335
Jan Heyd <git@janheyd.de>
2018-03-26 00:38:03 +0200

    improve toString verification failure message

--- src/main/java/de/janheyd/propertyverifier/feature/ToStringFeature.java
+++ src/main/java/de/janheyd/propertyverifier/feature/ToStringFeature.java
@@ -3,26 +3,43 @@ package de.janheyd.propertyverifier.feature;
 import java.util.List;
 import java.util.function.Function;
 
+import static java.lang.String.format;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
 public class ToStringFeature<T, U> implements ObjectFeature<T, U> {
 
 	@Override
 	public void verify(Function<U, T> constructor, List<U> values) {
-		if (!(sameValueProducesSameToString(constructor, values)
-				&& differentValuesProduceDifferentToString(constructor, values)))
-			throw new AssertionError("Constructor <=> ToString verification failed");
+		verifySameValueProducesSameToString(constructor, values);
+		verifyDifferentValuesProduceDifferentToString(constructor, values);
 	}
 
-	private boolean sameValueProducesSameToString(Function<U, T> constructor, List<U> values) {
+	private void verifySameValueProducesSameToString(Function<U, T> constructor, List<U> values) {
 		Function<U, String> function = constructor.andThen(Object::toString);
-		return values.stream().allMatch(value -> function.apply(value).equals(function.apply(value)));
+		List<U> brokenValues = values.stream()
+				.filter(value -> !function.apply(value).equals(function.apply(value)))
+				.collect(toList());
+		if (!brokenValues.isEmpty()) {
+			String explanation = "equal property values should produce equal toString values";
+			String header = "ToString verification failed (" + explanation + "):\n";
+			String template = "toString of object with property value %s is not equal to toString of another object "
+					+ "with same property value";
+			String details = brokenValues.stream()
+					.map(value -> format(template, value))
+					.collect(joining("\n"));
+			throw new AssertionError(header + details);
+		}
 	}
 
-	private boolean differentValuesProduceDifferentToString(Function<U, T> constructor, List<U> values) {
+	private void verifyDifferentValuesProduceDifferentToString(Function<U, T> constructor, List<U> values) {
 		long uniqueToStringValues = values.stream()
 				.map(constructor)
 				.map(Object::toString)
 				.distinct().count();
-		return uniqueToStringValues == values.size();
+		if (uniqueToStringValues != values.size())
+			throw new AssertionError("Constructor <=> ToString verification failed");
 	}
 
 }
--- src/test/java/de/janheyd/propertyverifier/ToStringTest.java
+++ src/test/java/de/janheyd/propertyverifier/ToStringTest.java
@@ -1,27 +1,48 @@
 package de.janheyd.propertyverifier;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+
 
 public class ToStringTest {
 
+	@Rule
+	public ExpectedException expected = ExpectedException.none();
+
 	@Test
-	public void canVerifyConstructorAndGetter() throws Exception {
+	public void canVerifyConstructorAndToString() throws Exception {
 		new PropertyVerifier()
 				.withConstructor(HasToString::new)
 				.withToString()
 				.verify(0, 1);
 	}
 
-	@Test(expected = AssertionError.class)
+	@Test
 	public void canDetectConstantToString() throws Exception {
+		expected.expect(AssertionError.class);
+		// TODO improve this message
+		expected.expectMessage(equalTo("Constructor <=> ToString verification failed"));
+
 		new PropertyVerifier()
 				.withConstructor(HasConstantToString::new)
 				.withToString()
 				.verify(0, 1);
 	}
 
-	@Test(expected = AssertionError.class)
+	@Test
 	public void canDetectDefaultToString() throws Exception {
+		expected.expect(AssertionError.class);
+		expected.expectMessage(equalTo(
+				"ToString verification failed (equal property values should produce equal toString values):\n"
+						+ "toString of object with property value 0 is not equal to toString of another object with "
+						+ "same property value\n"
+						+ "toString of object with property value 1 is not equal to toString of another object with "
+						+ "same property value"
+		));
+
 		new PropertyVerifier()
 				.withConstructor(HasDefaultToString::new)
 				.withToString()
